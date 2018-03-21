778c7a49dc92156c4f3fbb846555db12b8491f30
Roel <roelbrandenburg@hotmail.com>
2018-03-21 14:12:25 +0100

    added dynamic videoview

--- TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/information/VideoGridActivity.java
+++ TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/information/VideoGridActivity.java
@@ -1,18 +1,34 @@
 package com.rugged.tuberculosisapp.information;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import com.rugged.tuberculosisapp.R;
 
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
 public class VideoGridActivity extends AppCompatActivity {
 
+    private GridView gridView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_grid);
+
+        // Get list view
+        gridView = findViewById(R.id.videoGrid);
 
         // Get intent that started this activity
         Intent intent = getIntent();
@@ -24,5 +40,11 @@ public class VideoGridActivity extends AppCompatActivity {
 
         // TODO: implement a grid adapter that populates grid with clickable video thumbnails (YouTube API)
         // To get video URL list: intent.getStringArrayListExtra(TabInformation.VIDEO_URLS_MESSAGE)..
+
+        ArrayList<String> videoUrls = intent.getStringArrayListExtra(TabInformation.VIDEO_URLS_MESSAGE);
+        VideoGridAdapter adapter = new VideoGridAdapter(this, R.layout.thumbnail_holder, videoUrls);
+
+        // Get video grid view
+        gridView.setAdapter(adapter);
     }
 }

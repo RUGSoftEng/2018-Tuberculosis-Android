77becb5c68b51f6a873f5a687be3fb6e41ea10a0
rriesebos <rriesebos@hotmail.com>
2018-03-28 20:59:59 +0200

    Added internet permission to AndroidManifest for loading youtube video's, implemented thumbnail loading in the video grid, implemented embedded youtube video's in the quiz activity, renamed some non-descriptive views

--- TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/information/VideoGridAdapter.java
+++ TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/information/VideoGridAdapter.java
@@ -2,41 +2,97 @@ package com.rugged.tuberculosisapp.information;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.widget.BaseAdapter;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubeThumbnailLoader;
+import com.google.android.youtube.player.YouTubeThumbnailView;
 import com.rugged.tuberculosisapp.R;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class VideoGridAdapter extends ArrayAdapter<String> {
+public class VideoGridAdapter extends BaseAdapter implements YouTubeThumbnailView.OnInitializedListener {
 
     private final Context mContext;
-    private final int mResourceId;
     private final List<String> videoUrls;
+    private Map<View, YouTubeThumbnailLoader> loaders;
 
-    VideoGridAdapter(Context context, int resourceId, List<String> videoUrls) {
-        super(context, resourceId, videoUrls);
+    VideoGridAdapter(Context context, List<String> videoUrls) {
         this.mContext = context;
-        this.mResourceId = resourceId;
         this.videoUrls = videoUrls;
+        this.loaders = new HashMap<>();
+    }
+
+    @Override
+    public int getCount() {
+        return videoUrls.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return videoUrls.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        // TODO: use videoUrl to retrieve thumbnail
-        String videoUrl = getItem(position);
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        YouTubeThumbnailView youTubeThumbnailView;
+
+        // Get videoUrl of grid item
+        final String videoUrl = getItem(position);
 
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.thumbnail_holder, parent, false);
+            // Initialize loader
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.thumbnail_holder, parent, false);
+
+            youTubeThumbnailView = convertView.findViewById(R.id.thumbnailView);
+            youTubeThumbnailView.setTag(videoUrl);
+
+            youTubeThumbnailView.initialize(TabInformation.DEVELOPER_KEY, this);
+        } else {
+            youTubeThumbnailView = convertView.findViewById(R.id.thumbnailView);
+            YouTubeThumbnailLoader loader = loaders.get(youTubeThumbnailView);
+
+            if (loader == null) {
+                // Loader is busy initializing
+                youTubeThumbnailView.setTag(videoUrl);
+            } else {
+                // The loader is already initialized
+                // TODO: add loading image
+                loader.setVideo(videoUrl);
+            }
         }
 
         return convertView;
     }
+
+    @Override
+    public void onInitializationSuccess(YouTubeThumbnailView youTubeThumbnailView, final YouTubeThumbnailLoader youTubeThumbnailLoader) {
+        loaders.put(youTubeThumbnailView, youTubeThumbnailLoader);
+        youTubeThumbnailLoader.setVideo((String) youTubeThumbnailView.getTag());
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubeThumbnailView youTubeThumbnailView, YouTubeInitializationResult youTubeInitializationResult) {
+        //TODO: implement error handling
+    }
+
+    public void releaseLoaders() {
+        for (YouTubeThumbnailLoader loader : loaders.values()) {
+            loader.release();
+        }
+    }
+
 }

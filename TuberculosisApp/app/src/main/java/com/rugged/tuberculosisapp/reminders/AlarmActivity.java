2e553bad80fdd16b61da3e2408b9369661bd66e5
Roel <roelbrandenburg@hotmail.com>
2018-04-02 12:51:42 +0200

    Alarm reminder go to calendar button works now

--- TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/reminders/AlarmActivity.java
+++ TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/reminders/AlarmActivity.java
@@ -1,10 +1,15 @@
 package com.rugged.tuberculosisapp.reminders;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.rugged.tuberculosisapp.MainActivity;
 import com.rugged.tuberculosisapp.R;
+import com.rugged.tuberculosisapp.calendar.TabCalendar;
+import com.rugged.tuberculosisapp.settings.LanguageSelect;
+import com.rugged.tuberculosisapp.signin.SignInActivity;
 
 public class AlarmActivity extends AppCompatActivity {
 
@@ -16,5 +21,10 @@ public class AlarmActivity extends AppCompatActivity {
 
     public void close(View view) {
         finish();
+    }
+
+    public void toCalendar(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }

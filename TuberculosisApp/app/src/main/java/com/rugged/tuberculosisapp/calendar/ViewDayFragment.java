a44a9c00283f2a59b992a9b06373fc827d3e6deb
rriesebos <rriesebos@hotmail.com>
2018-04-02 22:23:11 +0200

    Fixed dialog title showing, fixed back button not working in settings, fixed password field getting cut off

--- TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/calendar/ViewDayFragment.java
+++ TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/calendar/ViewDayFragment.java
@@ -1,11 +1,13 @@
 package com.rugged.tuberculosisapp.calendar;
 
+import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -29,6 +31,7 @@ public class ViewDayFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_view_day, container, false);
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         TextView titleMedicationDialog = view.findViewById(R.id.titleMedicationDialog);
         ListView medicationListView = view.findViewById(R.id.medicationListDialog);
--- TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/information/QuizFragment.java
+++ TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/information/QuizFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -21,6 +22,7 @@ public class QuizFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_quiz_answers, container, false);
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         ListView quizListView = view.findViewById(R.id.answersList);
         Button dismissButton = view.findViewById(R.id.buttonDismiss);

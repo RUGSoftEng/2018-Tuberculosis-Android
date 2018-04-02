02942ffa0fd4db5d47f1c72d9ab5692feda21413
rriesebos <rriesebos@hotmail.com>
2018-04-02 20:00:16 +0200

    Some bugfixes

--- TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/information/QuizActivity.java
+++ TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/information/QuizActivity.java
@@ -1,6 +1,5 @@
 package com.rugged.tuberculosisapp.information;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,13 +12,13 @@ import com.google.android.youtube.player.YouTubePlayerFragment;
 import com.rugged.tuberculosisapp.R;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class QuizActivity extends AppCompatActivity {
-    static final int size = 4;
-    boolean[] correct = new boolean[size];
+    static final int nrOfQuestions = 4;
+    boolean[] correct = new boolean[nrOfQuestions];
     ArrayList<String> array;
     private static String videoUrl;
-    private Activity mActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,56 +49,62 @@ public class QuizActivity extends AppCompatActivity {
                         //TODO: error handling
                     }
                 });
+
+        Arrays.fill(correct, false);
     }
-   public void onRadioButtonClicked(View view) {
+
+    public void onRadioButtonClicked(View view) {
         // Is the button now checked?
        boolean checked = ((RadioButton) view).isChecked();
        switch(view.getId()) {
            case R.id.correct_answer1:
                if (checked)
-                   correct[0]=true;
+                   correct[0] = true;
                    break;
            case R.id.correct_answer2:
                if (checked)
-                   correct[1]=true;
+                   correct[1] = true;
                    break;
            case R.id.correct_answer3:
                if (checked)
-                   correct[2]=true;
+                   correct[2] = true;
                    break;
            case R.id.correct_answer4:
                if (checked)
-                   correct[3]=true;
+                   correct[3] = true;
                    break;
            case R.id.wrong_answer1:
                if (checked)
-                   correct[0]=false;
+                   correct[0] = false;
                    break;
            case R.id.wrong_answer2:
                if (checked)
-                   correct[1]=false;
+                   correct[1] = false;
                    break;
            case R.id.wrong_answer3:
                if (checked)
-                   correct[2]=false;
+                   correct[2] = false;
                    break;
            case R.id.wrong_answer4:
                if (checked)
-                   correct[3]=false;
+                   correct[3] = false;
                    break;
        }
-   }
-   public void submitQuiz(View view){
+    }
+
+    public void submitQuiz(View view){
         array = new ArrayList<String>();
-        for(int i = 0; i < size;i++){
-            if(correct[i]){
-               array.add("Your answer for question " + i + "is correct");
+        for (int i = 0; i < nrOfQuestions; i++) {
+            if (correct[i]) {
+                array.add("Your answer for question " + i + "is correct");
             } else {
                 array.add("Your answer for question " + i + "is wrong");
             }
         }
+
         QuizFragment quizfragment = new QuizFragment();
         quizfragment.setChecked(array);
-       quizfragment.show(mActivity.getFragmentManager(), "QuizFragment");
-   }
+        quizfragment.show(getFragmentManager(), "QuizFragment");
+    }
+
 }
--- TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/information/QuizFragment.java
+++ TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/information/QuizFragment.java
@@ -9,17 +9,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.rugged.tuberculosisapp.R;
-import com.rugged.tuberculosisapp.medication.Medication;
-import com.rugged.tuberculosisapp.medication.MedicationListAdapter;
-import com.rugged.tuberculosisapp.settings.LanguageHelper;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
 
 public class QuizFragment extends DialogFragment {
     private ArrayList<String> checked;
@@ -29,9 +22,10 @@ public class QuizFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_quiz_answers, container, false);
 
-        ListView quizListView = view.findViewById(R.id.quizList);
+        ListView quizListView = view.findViewById(R.id.answersList);
         Button dismissButton = view.findViewById(R.id.buttonDismiss);
-       ArrayAdapter adapter = new ArrayAdapter<String>(this.getActivity(), R.layout.quizlist, checked);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.answer_row, checked);
         quizListView.setAdapter(adapter);
 
         dismissButton.setOnClickListener(new View.OnClickListener() {

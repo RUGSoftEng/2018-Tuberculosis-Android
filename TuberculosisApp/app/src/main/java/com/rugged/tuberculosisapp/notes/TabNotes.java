e07f3d355797cd301b15fe759f3a04186c1f9f69
Roel <roelbrandenburg@hotmail.com>
2018-04-23 10:06:45 +0200

    changes notes layout, only need to dynamically add text questions to the FAQ entries

--- TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/notes/TabNotes.java
+++ TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/notes/TabNotes.java
@@ -6,18 +6,56 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.rugged.tuberculosisapp.R;
+import com.rugged.tuberculosisapp.information.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TabNotes extends Fragment {
 
     public static final String TITLE = "TabNotes";
 
+    private LinearLayout entries;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_tab_notes, container, false);
+
+        // Get list view
+        entries = view.findViewById(R.id.FAQEntries);
+
+        // Prepare categories
+        prepareListData();
+
         return view;
     }
 
+    private void prepareListData() {
+        TextView question1 = new TextView(getActivity());
+        question1.setId(View.generateViewId());
+        question1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        question1.setText(getText(R.string.lorem_ipsum));
+
+        TextView question2 = new TextView(getActivity());
+        question1.setId(View.generateViewId());
+        question1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        question1.setText(getText(R.string.lorem_ipsum));
+
+        TextView question3 = new TextView(getActivity());
+        question1.setId(View.generateViewId());
+        question1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        question1.setText(getText(R.string.lorem_ipsum));
+
+        entries.addView(question1);
+        entries.addView(question2);
+        entries.addView(question3);
+    }
 }

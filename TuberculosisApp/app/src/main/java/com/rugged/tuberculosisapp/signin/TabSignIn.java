4f8cf7d8d74b5705030ac752a80deda2d84e0ab8
Roel <roelbrandenburg@hotmail.com>
2018-04-29 15:27:22 +0200

    now shows a proper message when entering invalid credentials

--- TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/signin/TabSignIn.java
+++ TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/signin/TabSignIn.java
@@ -1,9 +1,9 @@
 package com.rugged.tuberculosisapp.signin;
 
 import android.graphics.Color;
-import android.graphics.ColorSpace;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +14,7 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.rugged.tuberculosisapp.MainActivity;
@@ -23,9 +23,14 @@ import com.rugged.tuberculosisapp.MainActivity;
 public class TabSignIn extends Fragment {
 
     public static final String TITLE = "TabSignIn";
+    // TODO remove DUMMY_ACCOUNTS when authentication goes through database
     private static final Account[] DUMMY_ACCOUNTS = new Account[] {
-        new Account("bar", "42"),
-        new Account("foo", "21")
+        new Account("admin", "admin"),
+        new Account("marco", "marco"),
+        new Account("niek", "niek"),
+        new Account("pj", "pj"),
+        new Account("robert", "robert"),
+        new Account("roel", "roel")
     };
 
     @Nullable
@@ -36,28 +41,33 @@ public class TabSignIn extends Fragment {
         Button signInButton = view.findViewById(R.id.signInButton);
         final EditText textUsername = view.findViewById(R.id.textUsername);
         final EditText textPassword = view.findViewById(R.id.textPassword);
-        final RelativeLayout parentView = view.findViewById(R.id.signInParent);
+        final LinearLayout invalidText = view.findViewById(R.id.invalidText);
+        invalidText.removeAllViews();
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = textUsername.getText().toString();
                 String password = textPassword.getText().toString();
+                invalidText.removeAllViews();
                 if (authenticate(new Account(username, password))) {
                     Intent intent = new Intent(getActivity(), MainActivity.class);
                     startActivity(intent);
                 } else {
                     TextView invalid = new TextView(getActivity());
-                    invalid.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+                    invalid.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
                     invalid.setText(getText(R.string.invalidLogIn));
                     invalid.setTextColor(Color.RED);
-                    parentView.addView(invalid);
+                    invalid.setGravity(Gravity.CENTER);
+                    invalidText.addView(invalid);
+                    textUsername.setText("");
+                    textPassword.setText("");
                 }
             }
         });
         return view;
     }
 
-    // TODO change authentication function to call database
+    // TODO change authentication function to call database (API)
     private boolean authenticate(Account account) {
         for (int i = 0; i < DUMMY_ACCOUNTS.length; i++) {
             if (account.getUsername().equals(DUMMY_ACCOUNTS[i].getUsername())

a33895864c0bbf72490612a44e701b5999bc9743
Roel <roelbrandenburg@hotmail.com>
2018-05-15 15:04:41 +0200

    Question is now clickable and shows text

--- TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/notes/TabNotes.java
+++ TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/notes/TabNotes.java
@@ -29,6 +29,8 @@ import static com.rugged.tuberculosisapp.MainActivity.ENABLE_API;
 public class TabNotes extends Fragment {
 
     public static final String TITLE = "TabNotes";
+    private static final int NO_LAST_QUESTION = -1;
+    private int questionIdx = NO_LAST_QUESTION;
 
     private LinearLayout entries;
 
@@ -59,6 +61,12 @@ public class TabNotes extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        questionIdx = NO_LAST_QUESTION;
     }
 
     private void sendQuestion(String question, final EditText editText) {
@@ -113,9 +121,49 @@ public class TabNotes extends Fragment {
         int numOfEntries = 10; //TODO get entries from API call
 
         for (int i = 0; i < numOfEntries; i++) {
-            TextView question = new TextView(getActivity());
+            final TextView question = new TextView(getActivity());
             question.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             question.setText(getText(R.string.lorem_ipsum)); //TODO get strings from API call
+            if (i > 0) { // Add distance between questions
+                question.setPadding(0, 24, 0, 0);
+            }
+            question.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int elementIdx = entries.indexOfChild(question);
+
+                    if (elementIdx == questionIdx) { // Clicking the same question
+                        question.setText(R.string.lorem_ipsum);
+                        entries.removeViewAt(elementIdx+1);
+                        questionIdx = NO_LAST_QUESTION;
+                    } else if (questionIdx == NO_LAST_QUESTION) { // No question has been selected previously
+                        question.setText("I am clicked yo");
+
+                        final TextView answer = new TextView(getActivity());
+                        answer.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                        answer.setText("testy answer");
+                        entries.addView(answer, elementIdx+1);
+
+                        questionIdx = elementIdx;
+                    } else { // Clicking on a different question
+                        question.setText("I am clicked yo2");
+                        TextView previousQuestionText = (TextView) entries.getChildAt(questionIdx);
+                        previousQuestionText.setText(R.string.lorem_ipsum);
+
+                        entries.removeViewAt(questionIdx+1);
+                        if (questionIdx < elementIdx) {
+                            elementIdx--;
+                        }
+
+                        final TextView answer = new TextView(getActivity());
+                        answer.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                        answer.setText("testy answer2");
+                        entries.addView(answer, elementIdx+1);
+
+                        questionIdx = elementIdx;
+                    }
+                }
+            });
             entries.addView(question);
         }
     }

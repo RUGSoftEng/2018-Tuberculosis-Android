070924a2ba994cf28a9f402579903ece1b883799
marco lu <marcoluutje@gmail.com>
2018-05-16 09:52:09 +0200

    temp

--- TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/network/RetrofitClientInstance.java
+++ TuberculosisApp/app/src/main/java/com/rugged/tuberculosisapp/network/RetrofitClientInstance.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientInstance {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://192.168.50.4:2002/api/"; // TODO change when database is stored on server
+    private static final String BASE_URL = "http://37.97.185.127:10123/api/"; // TODO change when database is stored on server
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
